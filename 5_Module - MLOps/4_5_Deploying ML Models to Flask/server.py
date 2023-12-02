4af6f30140c396c6480e36d8b755eddbe553fa13
Himanshu <himanshuramchandani08@gmail.com>
2023-12-02 10:24:16 +0530

    day 5 mlops flask

---
+++ 5_Module - MLOps/4_5_Deploying ML Models to Flask/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import joblib
+# import numpy as np
+
+app = Flask(__name__)
+
+model = joblib.load(open('models/text_classification_model.joblib', 'rb'))
+
+
+@app.route('/api', methods=['POST'])
+def predict():
+    data = 'Love in the Time of Money is a visually stunning film to watch'
+    prediction = model.predict(data)
+    output_text = "Text:" + str(data)
+    output = "Class:" +  str(prediction)
+    return jsonify(output_text, output)
+
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to the Text Classification</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
